4d360472c938acabfd031a29c2d5292a1ae885b4
sunggyeol <sunggyeol@vt.edu>
2024-07-14 17:23:17 +0900

    Modified header and logo

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Blog' },
 ]
 
 export default headerNavLinks
