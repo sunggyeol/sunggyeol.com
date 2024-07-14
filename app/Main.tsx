3887d1f99efa0eced10a0ac661bb689e53a93180
sunggyeol <sunggyeol@vt.edu>
2024-07-14 19:00:27 +0900

    Modified main page

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,12 +12,6 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Sung Oh
-          </h1>
-          <h2 className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Software Engineer Intern, Undergraduate Researcher, Founder of Fintellection, and a CS Student
-          </h2>
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
