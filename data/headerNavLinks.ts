dd2320ddc6a875d462dc5708fb784c3a466a4961
sunggyeol <sunggyeol@vt.edu>
2024-07-15 19:15:50 +0900

    Modified bio

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
 ]
 
 export default headerNavLinks
