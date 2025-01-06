7b5c8d0d1b5dd4f5d36dd239dfbb193e366b40bb
Sunggyeol(Paul) Oh <sunggyeol@vt.edu>
2025-01-06 18:23:23 +0900

    Update GoogleTagManager.tsx

--- components/GoogleTagManager.tsx
+++ components/GoogleTagManager.tsx
@@ -1,31 +1,31 @@
 import Script from "next/script";
 
 export function GoogleTagManagerHead() {
-    return (
-        <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NF82FN4S');`,
-            }}
-        />
-    );
+  return (
+    <Script
+      id="gtm-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NF82FN4S');`,
+      }}
+    />
+  );
 }
 
 export function GoogleTagManagerBody() {
-    return (
-        <noscript>
-            <iframe
-                title="Google Tag Manager"
-                src="https://www.googletagmanager.com/ns.html?id=GTM-NF82FN4S"
-                height="0"
-                width="0"
-                style={{ display: "none", visibility: "hidden" }}
-            />
-        </noscript>
-    );
+  return (
+    <noscript>
+      <iframe
+        title="Google Tag Manager"
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NF82FN4S"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
+  );
 }
