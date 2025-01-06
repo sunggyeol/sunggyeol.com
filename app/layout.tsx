7825779af8f2c9bfc4684c836a9ce8a3552dce06
Sunggyeol(Paul) Oh <sunggyeol@vt.edu>
2025-01-06 18:10:47 +0900

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import siteMetadata from "@/data/siteMetadata";
 import { ThemeProviders } from "./theme-providers";
 import { Metadata } from "next";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { GoogleTagManagerHead, GoogleTagManagerBody } from "@/components/GoogleTagManager";
 
 const space_grotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -70,42 +71,46 @@ export default function RootLayout({
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/static/favicons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/static/favicons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/static/favicons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/static/favicons/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/static/favicons/safari-pinned-tab.svg"
-        color="#5bbad5"
-      />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta
-        name="theme-color"
-        media="(prefers-color-scheme: light)"
-        content="#fff"
-      />
-      <meta
-        name="theme-color"
-        media="(prefers-color-scheme: dark)"
-        content="#000"
-      />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <head>
+        <GoogleTagManagerHead />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/static/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#fff"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#000"
+        />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      </head>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+        <GoogleTagManagerBody />
         <ThemeProviders>
           <Analytics
             analyticsConfig={siteMetadata.analytics as AnalyticsConfig}
